fe1657e8952cbaf91e0eee58d20ab0d0ce8e945a
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2018-10-17 23:54:51 +0200

    Adding data file

---
+++ data.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+// This is the file where you can keep your data arrays such as articles and
+// authors.
+//
+//
+// $authors = [
+//     ['name' => 'Andie MacDowell', 'ID' => 1],
+//     ['name' => 'Bill Murray', 'ID' => 2],
+//     ['name' => 'Chris Elliot', 'ID' => 3],
+//     ['name' => 'Stephen Tobowsky', 'ID' => 4],
+//     ['name' => 'Stephen Tobowsky', 'ID' => 5],
+// ];
+//
+
+
+$NewsFeed = [
+    [
+        'ID' => 1,
+        'Author' => 'Andie MacDowell',
+        'title' => 'Standing Up in the Milky Way',
+        'date' => '',
+        'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+         weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+         Doc, I\'m from the future. I came here in a time machine that you invented.
+         Now, I need your help to get back to the year 1985.',
+         'likes' => 22
+     ],
+
+     [
+         'ID' => 1,
+         'Author' => 'Andie MacDowell',
+         'title' => 'Standing Up in the Milky Way',
+         'date' => '',
+         'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+          weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+          Doc, I\'m from the future. I came here in a time machine that you invented.
+          Now, I need your help to get back to the year 1985.',
+          'likes' => 22
+      ],
+
+      [
+          'ID' => 2,
+          'Author' => 'Bill Murray',
+          'title' => 'Standing Up in the Milky Way',
+          'date' => '',
+          'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+           weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+           Doc, I\'m from the future. I came here in a time machine that you invented.
+           Now, I need your help to get back to the year 1985.',
+           'likes' => 22
+       ],
+
+       [
+           'ID' => 2,
+           'Author' => 'Bill Murray',
+           'title' => 'Standing Up in the Milky Way',
+           'date' => '',
+           'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+            weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+            Doc, I\'m from the future. I came here in a time machine that you invented.
+            Now, I need your help to get back to the year 1985.',
+            'likes' => 22
+        ],
+
+        [
+            'ID' => 3,
+            'Author' => 'Chris Elliot',
+            'title' => 'Standing Up in the Milky Way',
+            'date' => '',
+            'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+             weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+             Doc, I\'m from the future. I came here in a time machine that you invented.
+             Now, I need your help to get back to the year 1985.',
+             'likes' => 22
+         ],
+
+         [
+             'ID' => 3,
+             'Author' => 'Chris Elliot',
+             'title' => 'Standing Up in the Milky Way',
+             'date' => '',
+             'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+              weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+              Doc, I\'m from the future. I came here in a time machine that you invented.
+              Now, I need your help to get back to the year 1985.',
+              'likes' => 22
+          ],
+
+          [
+              'ID' => 4,
+              'Author' => 'Stephen Tobowsky',
+              'title' => 'Standing Up in the Milky Way',
+              'date' => '',
+              'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+               weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+               Doc, I\'m from the future. I came here in a time machine that you invented.
+               Now, I need your help to get back to the year 1985.',
+               'likes' => 22
+           ],
+
+           [
+               'ID' => 4,
+               'Author' => 'Stephen Tobowsky',
+               'title' => 'Standing Up in the Milky Way',
+               'date' => '',
+               'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+                weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+                Doc, I\'m from the future. I came here in a time machine that you invented.
+                Now, I need your help to get back to the year 1985.',
+                'likes' => 22
+            ],
+
+            [
+                'ID' => 5,
+                'Author' => 'Andie MacDowell',
+                'title' => 'Standing Up in the Milky Way',
+                'date' => '',
+                'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+                 weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+                 Doc, I\'m from the future. I came here in a time machine that you invented.
+                 Now, I need your help to get back to the year 1985.',
+                 'likes' => 22
+             ],
+
+             [
+                 'ID' => 5,
+                 'Author' => 'Andie MacDowell',
+                 'title' => 'Standing Up in the Milky Way',
+                 'date' => '',
+                 'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+                  weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+                  Doc, I\'m from the future. I came here in a time machine that you invented.
+                  Now, I need your help to get back to the year 1985.',
+                  'likes' => 22
+              ],
+
+          ];
+
+          var_dump($NewsFeed);
