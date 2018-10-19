9cf1897813d6537e3186e66e493c9cfa63954a43
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2018-10-19 04:15:19 +0200

    arrays fixed

--- data.php
+++ data.php
@@ -4,139 +4,150 @@ declare(strict_types=1);
 
 // This is the file where you can keep your data arrays such as articles and
 // authors.
-//
-//
-// $authors = [
-//     ['name' => 'Andie MacDowell', 'ID' => 1],
-//     ['name' => 'Bill Murray', 'ID' => 2],
-//     ['name' => 'Chris Elliot', 'ID' => 3],
-//     ['name' => 'Stephen Tobowsky', 'ID' => 4],
-//     ['name' => 'Stephen Tobowsky', 'ID' => 5],
-// ];
-//
-
 
 $NewsFeed = [
+
     [
-        'ID' => 1,
-        'Author' => 'Andie MacDowell',
-        'title' => 'Standing Up in the Milky Way',
-        'date' => '',
-        'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-         weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-         Doc, I\'m from the future. I came here in a time machine that you invented.
-         Now, I need your help to get back to the year 1985.',
-         'likes' => 22
-     ],
 
-     [
-         'ID' => 1,
-         'Author' => 'Andie MacDowell',
-         'title' => 'Standing Up in the Milky Way',
-         'date' => '',
-         'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-          weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-          Doc, I\'m from the future. I came here in a time machine that you invented.
-          Now, I need your help to get back to the year 1985.',
-          'likes' => 22
-      ],
+    'ID' => 1,
+    'Author' => 'Andie MacDowell',
+    'title' => 'Standing Up in the Milky Way',
+    'date' => '18 Oct 2018',
+    'Content' => 'Well, ma, we talked about this, we\'re not gonna go to the lake, the car\'s wrecked. Well, Marty,
+     I want to thank you for all your good advise, I\'ll never forget it. No no. Lorraine, Lorraine, what are you doing?
+     You wait and see, Mr. Caruthers, I will be mayor and I\'ll be the most powerful mayor in the history of Hill Valley,
+     and I\'m gonna clean up this town. Right.',
+    'likes' => 22
 
-      [
-          'ID' => 2,
-          'Author' => 'Bill Murray',
-          'title' => 'Standing Up in the Milky Way',
-          'date' => '',
-          'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-           weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-           Doc, I\'m from the future. I came here in a time machine that you invented.
-           Now, I need your help to get back to the year 1985.',
-           'likes' => 22
-       ],
+    ],
 
-       [
-           'ID' => 2,
-           'Author' => 'Bill Murray',
-           'title' => 'Standing Up in the Milky Way',
-           'date' => '',
-           'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-            weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-            Doc, I\'m from the future. I came here in a time machine that you invented.
-            Now, I need your help to get back to the year 1985.',
-            'likes' => 22
-        ],
+    [
 
-        [
-            'ID' => 3,
-            'Author' => 'Chris Elliot',
-            'title' => 'Standing Up in the Milky Way',
-            'date' => '',
-            'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-             weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-             Doc, I\'m from the future. I came here in a time machine that you invented.
-             Now, I need your help to get back to the year 1985.',
-             'likes' => 22
-         ],
+    'ID' => 2,
+    'Author' => 'Andie MacDowel',
+    'title' => 'Some of the Things That Molecules Do',
+    'date' => '9 dec 2018',
+    'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+    weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+    Doc, I\'m from the future. I came here in a time machine that you invented.
+    Now, I need your help to get back to the year 1985.',
+    'likes' => 22
 
-         [
-             'ID' => 3,
-             'Author' => 'Chris Elliot',
-             'title' => 'Standing Up in the Milky Way',
-             'date' => '',
-             'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-              weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-              Doc, I\'m from the future. I came here in a time machine that you invented.
-              Now, I need your help to get back to the year 1985.',
-              'likes' => 22
-          ],
+    ],
 
-          [
-              'ID' => 4,
-              'Author' => 'Stephen Tobowsky',
-              'title' => 'Standing Up in the Milky Way',
-              'date' => '',
-              'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-               weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-               Doc, I\'m from the future. I came here in a time machine that you invented.
-               Now, I need your help to get back to the year 1985.',
-               'likes' => 22
-           ],
+    [
 
-           [
-               'ID' => 4,
-               'Author' => 'Stephen Tobowsky',
-               'title' => 'Standing Up in the Milky Way',
-               'date' => '',
-               'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-                weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-                Doc, I\'m from the future. I came here in a time machine that you invented.
-                Now, I need your help to get back to the year 1985.',
-                'likes' => 22
-            ],
+    'ID' => 3,
+    'Author' => 'Bill Murray',
+    'title' => 'When Knowledge Conquered Fear',
+    'date' => '13 Jan 2018',
+    'Content' => 'My god, they found me. I don\'t know how but they found me. Run for it, Marty. My god, they found me.
+     I don\'t know how but they found me. Run for it, Marty. A colored mayor, that\'ll be the day. Hey Biff,
+     check out this guy\'s life preserver, dork thinks he\'s gonna drown. Do you mind if we park for a while?
+     Okay, that\'s enough. Now stop the microphone. I\'m sorry fellas. I\'m afraid you\'re just too darn loud.
+     Next, please. Where\'s the next group, please.',
+    'likes' => 22
 
-            [
-                'ID' => 5,
-                'Author' => 'Andie MacDowell',
-                'title' => 'Standing Up in the Milky Way',
-                'date' => '',
-                'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-                 weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-                 Doc, I\'m from the future. I came here in a time machine that you invented.
-                 Now, I need your help to get back to the year 1985.',
-                 'likes' => 22
-             ],
+    ],
 
-             [
-                 'ID' => 5,
-                 'Author' => 'Andie MacDowell',
-                 'title' => 'Standing Up in the Milky Way',
-                 'date' => '',
-                 'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-                  weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-                  Doc, I\'m from the future. I came here in a time machine that you invented.
-                  Now, I need your help to get back to the year 1985.',
-                  'likes' => 22
-              ],
+    [
 
-          ];
+    'ID' => 4,
+    'Author' => 'Bill Murray',
+    'title' => ' Sky Full of Ghosts',
+    'date' => '19 Jul 2018',
+    'Content' => 'Doc, look, all we need is a little plutonium. Nothing, nothing, nothing, look tell her destiny has
+     brought you together, tell her that she\'s the most beautiful you have ever seen. Girls like that stuff. What,
+     what are you doing George? Go. That\'s Calvin Klein, oh my god, he\'s a dream. Yes, definitely, god-dammit George,
+     swear. Okay, so now, you come up, you punch me in the stomach, I\'m out for the count, right? And you and Lorraine
+     live happily ever after.',
+    'likes' => 22
 
-          var_dump($NewsFeed);
+    ],
+
+    [
+
+    'ID' => 5,
+    'Author' => 'Chris Elliot',
+    'title' => 'Hiding in the Light',
+    'date' => '18 aug 2018',
+    'Content' => 'Well, ma, we talked about this, we\'re not gonna go to the lake, the car\'s wrecked. Well, Marty,
+     I want to thank you for all your good advise, I\'ll never forget it. No no. Lorraine, Lorraine, what are you doing?
+     You wait and see, Mr. Caruthers, I will be mayor and I\'ll be the most powerful mayor in the history of Hill Valley,
+     and I\'m gonna clean up this town. Right.',
+    'likes' => 22
+
+    ],
+
+    [
+
+    'ID' => 6,
+    'Author' => 'Chris Elliot',
+    'title' => 'Deeper, Deeper, Deeper Still',
+    'date' => '14 Oct 2018',
+    'Content' => 'My god, they found me. I don\'t know how but they found me. Run for it, Marty. My god, they found me.
+     I don\'t know how but they found me. Run for it, Marty. A colored mayor, that\'ll be the day. Hey Biff,
+     check out this guy\'s life preserver, dork thinks he\'s gonna drown. Do you mind if we park for a while?
+     Okay, that\'s enough. Now stop the microphone. I\'m sorry fellas. I\'m afraid you\'re just too darn loud.
+     Next, please. Where\'s the next group, please.',
+    'likes' => 22
+
+    ],
+
+    [
+
+    'ID' => 7,
+    'Author' => 'Stephen Tobowsky',
+    'title' => 'he Clean Room',
+    'date' => '3 Nov 2018',
+    'Content' => 'Doc, look, all we need is a little plutonium. Nothing, nothing, nothing, look tell her destiny has
+     brought you together, tell her that she\'s the most beautiful you have ever seen. Girls like that stuff. What,
+     what are you doing George? Go. That\'s Calvin Klein, oh my god, he\'s a dream. Yes, definitely, god-dammit George,
+     swear. Okay, so now, you come up, you punch me in the stomach, I\'m out for the count, right? And you and Lorraine
+     live happily ever after.',
+    'likes' => 22
+
+    ],
+
+    [
+
+    'ID' => 8,
+    'Author' => 'Stephen Tobowsky',
+    'title' => 'Sisters of the Sun',
+    'date' => '1 Jun 2018',
+    'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+    weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+    Doc, I\'m from the future. I came here in a time machine that you invented.
+    Now, I need your help to get back to the year 1985.',
+    'likes' => 22
+
+    ],
+
+    [
+
+    'ID' => 9,
+    'Author' => 'Andie MacDowell',
+    'title' => 'The Lost Worlds of Planet Earth',
+    'date' => '15 Feb 2018',
+    'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
+     weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
+     Doc, I\'m from the future. I came here in a time machine that you invented.
+     Now, I need your help to get back to the year 1985.',
+    'likes' => 22
+
+    ],
+
+    [
+
+     'ID' => 10,
+     'Author' => 'Andie MacDowell',
+     'title' => 'The Electric Boy',
+     'date' => '7 Mar 2018',
+     'Content' => 'What, well you mean like a date? Yoo. So what\'s it to you, butthead. You know you\'ve been looking for a,
+      since you\'re new here, I\'m gonna cut you a break, today. So why don\'t you make like a tree, and get out of here.
+      Well maybe you are and you just don\'t know it yet. You bet.',
+     'likes' => 22
+
+    ],
+
+];
