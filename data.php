f12cdfc3ab0f5753fcb7ffd2c8cfadc91a93cc73
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2018-10-28 21:02:41 +0100

    content variables added

--- data.php
+++ data.php
@@ -2,22 +2,86 @@
 
 declare(strict_types=1);
 
-// This is the file where you can keep your data arrays such as articles and
-// authors.
+// content variables//articles
+$standingUp =
 
-$NewsFeed = [
+     "Well, ma, we talked about this, we're not gonna go to the lake, the car's wrecked. Well, Marty,
+     I want to thank you for all your good advise, I'll never forget it. No no. Lorraine, Lorraine, what are you doing?
+     You wait and see, Mr. Caruthers, I will be mayor and I'll be the most powerful mayor in the history of Hill Valley,
+     and I'm gonna clean up this town. Right.";
+
+$someOfThe =
+
+    "Uh, Lorraine. How did you know I was here? Calvin. Oh no, don't touch that. That's some new specialized
+    weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you're alive. Do you know where 1640 Riverside-
+    Doc, I'm from the future. I came here in a time machine that you invented.
+    Now, I need your help to get back to the year 1985.";
+
+$whenKnowledge =
+
+    "Uh, Lorraine. How did you know I was here? Calvin. Oh no, don't touch that. That's some new specialized
+    weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you're alive. Do you know where 1640 Riverside-
+    Doc, I'm from the future. I came here in a time machine that you invented.
+    Now, I need your help to get back to the year 1985.";
+
+$skyFull =
+
+    "My god, they found me. I don't know how but they found me. Run for it, Marty.A colored mayor,
+     that'll be the day. Hey Biff, check out this guy's life preserver, dork thinks he's gonna drown. Do you mind if
+     we park for a while? Okay, that's enough. Now stop the microphone. I'm sorry fellas. I'm afraid you\'re just too
+     darn loud. Next, please. Where's the next group, please";
+
+$inTheLight =
+
+    "Well, ma, we talked about this, we're not gonna go to the lake, the car's wrecked. Well, Marty,
+     I want to thank you for all your good advise, I'll never forget it. No no. Lorraine, Lorraine, what are you doing?
+     You wait and see, Mr. Caruthers, I will be mayor and I'll be the most powerful mayor in the history of Hill Valley,
+     and I'm gonna clean up this town. Right.";
+
+$deeper =
+
+    "My god, they found me. I don't know how but they found me. Run for it, Marty. My god, they found me.
+     I don't know how but they found me. Run for it, Marty. A colored mayor, that'll be the day. Hey Biff,
+     check out this guy's life preserver, dork thinks he's gonna drown. Do you mind if we park for a while?
+     Okay, that's enough. Now stop the microphone.";
+
+$cleanRoom =
+
+    "Doc, look, all we need is a little plutonium. Nothing, nothing, nothing, look tell her destiny has
+     brought you together, tell her that she's the most beautiful you have ever seen. Girls like that stuff. What,
+     what are you doing George? Go. That's Calvin Klein, oh my god, he's a dream, know how but they found me. Run for
+     it, Marty.A colored mayor";
+
+$sisters =
+
+    "Lorraine. How did you know I was here? Calvin. Oh no, don't touch that. That's some new specialized
+    weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you're alive. Do you know where 1640 Riverside-
+    Doc, I'm from the future. I came here in a time machine that you invented.";
+
+$lostWorlds =
+
+    "Uh, Lorraine. How did you know I was here? Calvin. Oh no, don't touch that. That's some new specialized
+     weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you're alive. Do you know where 1640 Riverside-
+     Doc, I'm from the future.";
+
+$ewlectricBoy =
+
+    "What, well you mean like a date? Yoo. So what's it to you, butthead. You know you've been looking for a,
+     since you're new here, I'm gonna cut you a break, today. So why don't you make like a tree, and get out of here.
+     Well maybe you are and you just don't know it yet. You bet.";
+
+
+
+// News array
+$news= [
 
     [
 
     'ID' => 1,
     'Author' => 'Andie MacDowell',
     'title' => 'Standing Up in the Milky Way',
-    'date' => '18 Oct 2018',
-    'Content' => 'Well, ma, we talked about this, we\'re not gonna go to the lake, the car\'s wrecked. Well, Marty,
-     I want to thank you for all your good advise, I\'ll never forget it. No no. Lorraine, Lorraine, what are you doing?
-     You wait and see, Mr. Caruthers, I will be mayor and I\'ll be the most powerful mayor in the history of Hill Valley,
-     and I\'m gonna clean up this town. Right.',
-    'likes' => 22
+    'date' => '18 Oct 2018' ,
+    'Content' => $standingUp
 
     ],
 
@@ -26,12 +90,8 @@ $NewsFeed = [
     'ID' => 2,
     'Author' => 'Andie MacDowel',
     'title' => 'Some of the Things That Molecules Do',
-    'date' => '9 dec 2018',
-    'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-    weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-    Doc, I\'m from the future. I came here in a time machine that you invented.
-    Now, I need your help to get back to the year 1985.',
-    'likes' => 22
+    'date' => '9 Apr 2018',
+    'Content' => $someOfThe
 
     ],
 
@@ -41,12 +101,7 @@ $NewsFeed = [
     'Author' => 'Bill Murray',
     'title' => 'When Knowledge Conquered Fear',
     'date' => '13 Jan 2018',
-    'Content' => 'My god, they found me. I don\'t know how but they found me. Run for it, Marty. My god, they found me.
-     I don\'t know how but they found me. Run for it, Marty. A colored mayor, that\'ll be the day. Hey Biff,
-     check out this guy\'s life preserver, dork thinks he\'s gonna drown. Do you mind if we park for a while?
-     Okay, that\'s enough. Now stop the microphone. I\'m sorry fellas. I\'m afraid you\'re just too darn loud.
-     Next, please. Where\'s the next group, please.',
-    'likes' => 22
+    'Content' => $whenKnowledge
 
     ],
 
@@ -56,12 +111,7 @@ $NewsFeed = [
     'Author' => 'Bill Murray',
     'title' => ' Sky Full of Ghosts',
     'date' => '19 Jul 2018',
-    'Content' => 'Doc, look, all we need is a little plutonium. Nothing, nothing, nothing, look tell her destiny has
-     brought you together, tell her that she\'s the most beautiful you have ever seen. Girls like that stuff. What,
-     what are you doing George? Go. That\'s Calvin Klein, oh my god, he\'s a dream. Yes, definitely, god-dammit George,
-     swear. Okay, so now, you come up, you punch me in the stomach, I\'m out for the count, right? And you and Lorraine
-     live happily ever after.',
-    'likes' => 22
+    'Content' => $skyFull
 
     ],
 
@@ -71,11 +121,7 @@ $NewsFeed = [
     'Author' => 'Chris Elliot',
     'title' => 'Hiding in the Light',
     'date' => '18 aug 2018',
-    'Content' => 'Well, ma, we talked about this, we\'re not gonna go to the lake, the car\'s wrecked. Well, Marty,
-     I want to thank you for all your good advise, I\'ll never forget it. No no. Lorraine, Lorraine, what are you doing?
-     You wait and see, Mr. Caruthers, I will be mayor and I\'ll be the most powerful mayor in the history of Hill Valley,
-     and I\'m gonna clean up this town. Right.',
-    'likes' => 22
+    'Content' => $inTheLight
 
     ],
 
@@ -85,12 +131,7 @@ $NewsFeed = [
     'Author' => 'Chris Elliot',
     'title' => 'Deeper, Deeper, Deeper Still',
     'date' => '14 Oct 2018',
-    'Content' => 'My god, they found me. I don\'t know how but they found me. Run for it, Marty. My god, they found me.
-     I don\'t know how but they found me. Run for it, Marty. A colored mayor, that\'ll be the day. Hey Biff,
-     check out this guy\'s life preserver, dork thinks he\'s gonna drown. Do you mind if we park for a while?
-     Okay, that\'s enough. Now stop the microphone. I\'m sorry fellas. I\'m afraid you\'re just too darn loud.
-     Next, please. Where\'s the next group, please.',
-    'likes' => 22
+    'Content' => $deeper
 
     ],
 
@@ -98,14 +139,9 @@ $NewsFeed = [
 
     'ID' => 7,
     'Author' => 'Stephen Tobowsky',
-    'title' => 'he Clean Room',
+    'title' => 'He Clean Room',
     'date' => '3 Nov 2018',
-    'Content' => 'Doc, look, all we need is a little plutonium. Nothing, nothing, nothing, look tell her destiny has
-     brought you together, tell her that she\'s the most beautiful you have ever seen. Girls like that stuff. What,
-     what are you doing George? Go. That\'s Calvin Klein, oh my god, he\'s a dream. Yes, definitely, god-dammit George,
-     swear. Okay, so now, you come up, you punch me in the stomach, I\'m out for the count, right? And you and Lorraine
-     live happily ever after.',
-    'likes' => 22
+    'Content' => $cleanRoom
 
     ],
 
@@ -115,38 +151,27 @@ $NewsFeed = [
     'Author' => 'Stephen Tobowsky',
     'title' => 'Sisters of the Sun',
     'date' => '1 Jun 2018',
-    'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-    weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-    Doc, I\'m from the future. I came here in a time machine that you invented.
-    Now, I need your help to get back to the year 1985.',
-    'likes' => 22
+    'Content' => $sisters
 
     ],
 
     [
 
     'ID' => 9,
-    'Author' => 'Andie MacDowell',
+    'Author' => 'Emilio Estevez',
     'title' => 'The Lost Worlds of Planet Earth',
     'date' => '15 Feb 2018',
-    'Content' => 'Uh, Lorraine. How did you know I was here? Calvin. Oh no, don\'t touch that. That\'s some new specialized
-     weather sensing equipment. Brown, Brown, Brown, Brown, Brown, great, you\'re alive. Do you know where 1640 Riverside-
-     Doc, I\'m from the future. I came here in a time machine that you invented.
-     Now, I need your help to get back to the year 1985.',
-    'likes' => 22
+    'Content' => $lostWorlds
 
     ],
 
     [
 
      'ID' => 10,
-     'Author' => 'Andie MacDowell',
+     'Author' => 'Emilio Estevez',
      'title' => 'The Electric Boy',
      'date' => '7 Mar 2018',
-     'Content' => 'What, well you mean like a date? Yoo. So what\'s it to you, butthead. You know you\'ve been looking for a,
-      since you\'re new here, I\'m gonna cut you a break, today. So why don\'t you make like a tree, and get out of here.
-      Well maybe you are and you just don\'t know it yet. You bet.',
-     'likes' => 22
+     'Content' => $ewlectricBoy
 
     ],
 
