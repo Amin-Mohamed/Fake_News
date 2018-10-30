0a8f730ff91c3f65b888454322c292491292b002
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2018-10-30 14:31:12 +0100

    stuff fixed

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
@@ -13,7 +12,6 @@ require __DIR__.'/functions.php';
 usort($news, "sortByDate");
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
