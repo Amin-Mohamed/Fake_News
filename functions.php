1077d9871d7ef5ebba9376f903258cea347e57cd
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2018-11-02 01:01:48 +0100

    camelCase fixed

--- functions.php
+++ functions.php
@@ -3,25 +3,25 @@
 declare(strict_types=1);
 
 /**
- * [sortByDate by comparing two arrays]
- * @param  array $time_1 [description]
- * @param  array $time_2 [description]
- * @return bool          [description]
+ * [sortByDate description]
+ * @param  array $time1 [description]
+ * @param  array $time2 [description]
+ * @return bool         [description]
  */
 
-function sortByDate(array $time_1, array $time_2): bool
+function sortByDate(array $time1, array $time2): bool
 {
-    return strtotime($time_1['date']) < strtotime($time_2['date']);
+    return strtotime($time1['date']) < strtotime($time2['date']);
 }
 
 /**
- * [Generate a random likes number]
- * @param  int $min_likes [description]
- * @param  int $max_likes [description]
- * @return int      [description]
+ * [randomLikes description]
+ * @param  integer $minLikes [description]
+ * @param  integer $maxLikes [description]
+ * @return int               [description]
  */
 
-function randomLikes( int $min_likes = 5, int $max_likes = 200): int
+function randomLikes( int $minLikes = 1, int $maxLikes = 500): int
 {
-    return rand($min_likes, $max_likes);
+    return rand($minLikes, $maxLikes);
 }
